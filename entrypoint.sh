ca74a3fec0c7e7a3723f5b96a4dd17ff2c363556
Marvin Wendt <git@marvinjwendt.com>
2022-06-01 21:59:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,9 +49,9 @@ echo "# Running CI System"
 go run /main.go
 rm /main.go
 
-echo "## Generating changelog..."
-go install github.com/git-chglog/git-chglog/cmd/git-chglog@latest || true
-/github/home/go/bin/git-chglog -o CHANGELOG.md --config /.chglog/config.yml || true
+# echo "## Generating changelog..."
+# go install github.com/git-chglog/git-chglog/cmd/git-chglog@latest || true
+# /github/home/go/bin/git-chglog -o CHANGELOG.md --config /.chglog/config.yml || true
 
 echo "## Go mod tidy..."
 git checkout go.mod # reset go.mod file
