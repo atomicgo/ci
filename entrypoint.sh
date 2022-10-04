d6a5cb3fd1dc16b833958d8f65922323b7cd26df
Marvin Wendt <git@marvinjwendt.com>
2022-10-04 19:35:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,7 +46,7 @@ else
   echo "### Installing gomarkdoc..."
   go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@latest
   echo "### Running gomarkdoc..."
-  $(go env GOPATH)/bin/gomarkdoc -e -o README.md .
+  $(go env GOPATH)/bin/gomarkdoc --repository.url "https://github.com/$REPO_FULLNAME" --repository.default-branch main --repository.path / -e -o README.md .
 fi
 
 echo "# Running CI System"
