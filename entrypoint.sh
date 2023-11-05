661cf07fba92db6c5568d953550218b4591f022f
Marvin Wendt <git@marvinjwendt.com>
2023-11-05 03:54:15 +0100

    removed deprecated changelog

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,10 +54,6 @@ go get github.com/pterm/pterm
 go run /main.go
 rm /main.go
 
-# echo "## Generating changelog..."
-# go install github.com/git-chglog/git-chglog/cmd/git-chglog@latest || true
-# /github/home/go/bin/git-chglog -o CHANGELOG.md --config /.chglog/config.yml || true
-
 echo "## Go mod tidy..."
 git checkout go.mod # reset go.mod file
 go mod tidy
